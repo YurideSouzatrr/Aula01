06afd2b3e1a4cd4bcdd089f74d17b262d04768cf
YurideSouzatrr <72032198+YurideSouzatrr@users.noreply.github.com>
2020-11-25 11:00:04 -0300

    up ate que enfim

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? n = null;
+            long? p = null;
+            ushort? l = null;
+            double? k = null;
+            double? i = null;
+            double? y = 34;
+            double? b = null;
+          double? a = 65000000; 
+            ushort? t = 500;
+            double r = 7.50;
+            Console.WriteLine(i.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            if (b.HasValue)
+                Console.WriteLine(b.Value);
+            else
+                Console.WriteLine("");
+            if (a.HasValue)
+                Console.WriteLine(a.Value);
+            else
+                Console.WriteLine("");
+        }
+    }
+}

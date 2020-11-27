8ed814ed69ea576793b73b2f1d1f4c01857db2d3
YurideSouzatrr <72032198+YurideSouzatrr@users.noreply.github.com>
2020-11-27 13:23:09 -0300

    up2

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] v = new int[6];
+                int i; //contado do for.
+
+                for (i = 0; i < 6; i++)
+                {
+                    Console.Write("Digite os números : ");
+                    v[i] = Convert.ToInt16(Console.ReadLine());
+                    
+                }
+
+                Console.WriteLine(" Os números são : ");
+                for (i = 0; i < 6; i++)
+                {
+                    if (v[i] % 2 == 0) 
+                    {
+                        Console.WriteLine(v[i] + ":");
+                    }
+                    else
+                    {
+                        Console.WriteLine(v[i] + ":");
+                    }
+                }
+            }
+                }
+            }
+        }

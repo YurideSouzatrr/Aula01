793fa3aece57554f46e6b5635290945728dff9b8
YurideSouzatrr <72032198+YurideSouzatrr@users.noreply.github.com>
2020-12-01 20:05:33 -0300

    AP

---
+++ ReceitasCulinaria/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReceitasCulinaria
+{
+    class Pessoa
+    {
+   public string Cadastro { get; set; }
+    public string Editar { get; set; }
+    public string Excluir { get; set; }
+
+    }
+
+
+
+}

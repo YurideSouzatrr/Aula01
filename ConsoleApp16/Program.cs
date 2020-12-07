46cc83eccfef7f15d72e0d6cb52769870f25673a
YurideSouzatrr <72032198+YurideSouzatrr@users.noreply.github.com>
2020-12-07 14:16:51 -0300

    up

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list
+
+            List<string> nomesdelivros = new List<string>();
+
+            Console.Write(" Os Nomes dos Livros são : ");
+
+            nomesdelivros.Add(" | O Nome do Vento  |");
+            nomesdelivros.Add("| Agripa |");
+            nomesdelivros.Add("| Dom Quixote |");
+            nomesdelivros.Add("| 1822 |");
+            nomesdelivros.Add("| O Livro da Lua 2021 | ");
+            nomesdelivros.Add("| O Povo Brasileiro | ");
+            nomesdelivros.Add("| Jesus de Nazaré | ");
+            nomesdelivros.Add("| 1889 |");
+            nomesdelivros.Add("| O Naufrágio das Civilizações |");
+            nomesdelivros.Add("| 21 Lições do Século 21 |");
+
+            foreach (string w in nomesdelivros)
+            {
+                Console.WriteLine(w);
+                
+            }
+            Console.ReadLine();
+
+        }
+        }
+    }
+

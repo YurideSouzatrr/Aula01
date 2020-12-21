baf9f37eedc3fdd2e91be11294110a6596322714
YurideSouzatrr <72032198+YurideSouzatrr@users.noreply.github.com>
2020-12-21 14:22:17 -0300

    UP

---
+++ LISTA 5 PONTOS/ATV5/ATV5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ATV5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                int carrosVendidos;
+                string ler;
+                double comissao = 300.00, vlrComissao, salarioFixo = 800.00, salarioTotal;
+
+                Console.Write("Informe a qtde de carros vendidos:\n");
+                carrosVendidos = ler.nextInt();
+
+                vlrComissao = comissao * carrosVendidos;
+                salarioTotal = salarioFixo + vlrComissao;
+                Console.WriteLine("\n\nSalario Fixo   = R$ %8.2f", salarioFixo);
+                Console.WriteLine("\nValor Comissao = R$ %8.2f", vlrComissao);
+                Console.WriteLine("\n============================");
+                Console.WriteLine("\nSalario Total  = R$ %8.2f\n", salarioTotal);
+            }
+
+        }
+    }
+}
---
+++ LISTA 5 PONTOS/ATV7/ATV7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ATV7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int compra;
+            int prestacao;
+            int real;
+
+
+            Console.Write(" Loja Mamão com Açúcar ");
+            Console.WriteLine("Digite o valor da compra: ");
+            Console.WriteLine(compra);
+            prestacao < -compra / 5
+Console.WriteLine("O cliente deverá pagar 5 prestações de R$", prestacao, " cada");
+        }
+        }
+    }
